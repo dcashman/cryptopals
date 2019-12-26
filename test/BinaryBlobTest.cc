da62e55785147832724c1ac42f6fc57a8fcd230f
Daniel Cashman <dan.a.cashman@gmail.com>
2019-12-26 11:57:20 -0800

    Switch BinaryBlob test to use test fixture.
    Create a BinaryBlobTest class with a SetUp() method to allow deduplication of
    shared code when operating on text blobs.

--- test/BinaryBlobTest.cc
+++ test/BinaryBlobTest.cc
@@ -1,16 +1,24 @@
 #include "gtest/gtest.h"
 #include "src/BinaryBlob.h"
 
-TEST(BinaryBlob, getByteEmpty) {
-    BinaryBlob binaryBlob{};
-    EXPECT_EQ(0, binaryBlob.getByte(0));
+
+class BinaryBlobTest : public ::testing::Test {
+protected:
+ BinaryBlob binary_blob_;
+
+    void SetUp() override {
+        uint8_t data[4] = {0, 1, 2, 3};
+        binary_blob_ = BinaryBlob(data, 4);
+    }
+};
+
+TEST_F(BinaryBlobTest, getByteEmpty) {
+    BinaryBlob binary_blob{};
+    EXPECT_EQ(0, binary_blob.getByte(0));
 }
 
-TEST(BinaryBlob, getByte) {
-    // TODO: Move to setup func when used by other tests.
-    uint8_t data[4] = {0, 1, 2, 3};
-    BinaryBlob binaryBlob(data, 4);
+TEST_F(BinaryBlobTest, getByte) {
     for (int i = 0; i < 4; i++) {
-        EXPECT_EQ(i, binaryBlob.getByte(i));
+        EXPECT_EQ(i, binary_blob_.getByte(i));
     }
 }
